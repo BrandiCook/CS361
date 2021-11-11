5850030af4b3b506902b3b07787e5379d23497d4
BrandiCook <cookb4@oregonstate.edu>
2021-11-10 18:05:23 -0600

    more notes to user

--- TransService.py
+++ TransService.py
@@ -4,6 +4,8 @@ from googletrans import Translator
 # "pip install googletrans==3.1.0a0" is required before running this service
 
 # All you, as the user of this service, need to do is change the text in "request.txt" to whatever you want! :D
+# If you dont want it to print to the console, you can remove the print(text_to_translate) and
+# print(translated_text.text)
 
 
 # I used official information from the makers of google trans at https://py-googletrans.readthedocs.io/en/latest/
@@ -26,12 +28,14 @@ f = open("request.txt", 'r')
 a = open("response.txt", 'a')
 if f.mode == 'r':
     text_to_translate = f.read()
+    # Prints the text that will eventually be translated
     print(text_to_translate)
 
 translator = Translator()
 
 # translate contents of request.txt
 translated_text = translator.translate(text_to_translate, dest="es")
+# Prints the translated text
 print(translated_text.text)
 
 # More supported languages can be found at https://py-googletrans.readthedocs.io/en/latest/, just scroll down!
